763bef0521e6ea9e84b7583362ed8447be1a40f3
XYZ0031 <josephmajanja8@gmail.com>
2025-03-09 23:39:27 +0300

    Add files via upload

---
+++ Arrays Q1.cpp
@@ -0,0 +1,21 @@
+//c  ARRAYS
+/*
+c redefined functions
+Name:Joseph Majanja
+Reg no: BBIT-05-0031/2024
+Git username:XYZ0031
+Campus:TRC
+version
+*/
+#include<iostream>
+using namespace std;
+int main(){
+    int marks[6]={10,20,30,40,50,60};
+    int i;
+    for(i=0; i<6; i++){
+		 cout<<marks[i] <<"\t";
+		
+    
+    cout<<endl;}
+    return 0;
+}
