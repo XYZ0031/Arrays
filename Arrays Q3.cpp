b7aa038aed436f3f9a52e69ff03560a26708346a
XYZ0031 <josephmajanja8@gmail.com>
2025-03-09 23:38:20 +0300

    Add files via upload

---
+++ Arrays Q3.cpp
@@ -0,0 +1,27 @@
+//c 2D ARRAYS
+/*
+c redefined functions
+Name:Joseph Majanja
+Reg no: BBIT-05-0031/2024
+Git username:XYZ0031
+Campus:TRC
+version
+*/
+#include<iostream>
+using namespace std;
+int main(){
+    int marks[2][3]={
+	
+	{10,20,30},
+	 {40,50,60}
+	};
+    int i,j;
+    for(i=0; i<2; i++){
+    	for(j=0; j<3; j++){
+		
+        cout<<marks[i][j] <<"\t";
+		}
+    
+    cout<<endl;}
+    return 0;
+}
